13a476947592136545ddf2533ee13b2769ca3ec6
Dasgui <63924663+Dasgui@users.noreply.github.com>
2026-09-02 22:47:28 +0500

    Добавлен make файл с базовыми командами для запуска бд и сервера Добавлена функция вставляющая данные при создании таблицы

---
+++ Makefile
@@ -0,0 +1,16 @@
+all:
+	$(MAKE) init-db
+	$(MAKE) migrate
+	go run ./cmd/
+init-db:
+	docker compose up -d
+
+migrate:
+	goose up
+
+drop-bd:
+	goose down
+
+clean:
+	$(MAKE) drop-bd
+	docker compose down -v
--- internal/migrations/00001_create_products.sql
+++ internal/migrations/00001_create_products.sql
@@ -7,6 +7,10 @@ CREATE TABLE IF NOT EXISTS Products (
     category TEXT NOT NULL,
     created_at timestamp NOT NULL
 );
+INSERT INTO Products (name, description, price, category, created_at)
+VALUES('Test-1', 'Description Test-1', 2300, 'Test', '2026-09-02T11:37:16.77623'),
+('Test-2', 'Description Test-2', 6500, 'Test', '2026-09-03T11:37:16.77623'),
+('Test-3', 'Description Test-3', 8300, 'Test-3', '2026-09-04T11:37:16.77623');
 
 -- +goose Down
 DROP TABLE IF EXISTS Products;
